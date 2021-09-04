329da306b7cbcc03014b19998b65b6b8e6ca5f8b
Guilherme Lima <you@example.com>
2021-09-03 21:03:46 -0300

    finalizado skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 /* MENU SHOW & HIDDEN */
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+    const navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
 
     /* MENU SHOW */
     /* Validar se a constante existe */
@@ -30,4 +30,22 @@ const navMenu = document.getElementById('nav-menu'),
     }
     navLink.forEach(n => n.addEventListener('click', linkAction))
 
-    /*  */
+    /* FUNÇÃO ABRIR SKILLS  */
+    const skillsContent = document.getElementsByClassName('skills__content'),
+            skillsHeader = document.querySelectorAll('.skills__header')
+
+    function toggleSkills(){
+        let itemClass = this.parentNode.className
+
+        for(i = 0; i< skillsContent.length; i++){
+            skillsContent[i].className = 'skills__content skills__close'
+        }
+
+        if(itemClass === 'skills__content skills__close'){
+            this.parentNode.className = 'skills__content skills__open'
+        }
+    }
+
+    skillsHeader.forEach((el) =>{
+        el.addEventListener('click', toggleSkills)
+    })
