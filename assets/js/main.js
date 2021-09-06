a0fccd417ef264f4198c9f3420c852564098812c
Guilherme Lima <you@example.com>
2021-09-05 22:03:03 -0300

    finalizado portifolio com bugs na pagination

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,95 +1,111 @@
 /* MENU SHOW & HIDDEN */
-    const navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
-    /* MENU SHOW */
-    /* Validar se a constante existe */
-    if (navToggle) {
-        navToggle.addEventListener('click',() =>{
-            navMenu.classList.add('show-menu')
-        })
-    }
+/* MENU SHOW */
+/* Validar se a constante existe */
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
-    /* MENU HIDDEN */
-    /* Validar se a constante existe */
-    if(navClose){
-        navClose.addEventListener('click',() =>{
-            navMenu.classList.remove('show-menu')
-        })
-    }
-
-    /* REMOVER MENU MOBILE */
-    
-    const navLink = document.querySelectorAll('.nav__link')
-
-    function linkAction(){
-        const navMenu = document.getElementById('nav-menu')
-        // Esconder menu quando clica em algum link
+/* MENU HIDDEN */
+/* Validar se a constante existe */
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
-    }
-    navLink.forEach(n => n.addEventListener('click', linkAction))
-
-    /* FUNÇÃO ABRIR SKILLS  */
-    const skillsContent = document.getElementsByClassName('skills__content'),
-            skillsHeader = document.querySelectorAll('.skills__header')
-
-    function toggleSkills(){
-        let itemClass = this.parentNode.className
-
-        for(i = 0; i< skillsContent.length; i++){
-            skillsContent[i].className = 'skills__content skills__close'
-        }
-
-        if(itemClass === 'skills__content skills__close'){
-            this.parentNode.className = 'skills__content skills__open'
-        }
-    }
-
-    skillsHeader.forEach((el) =>{
-        el.addEventListener('click', toggleSkills)
     })
+}
 
-    /* GUIAS DE QUALIFICAÇÕES */ 
-    const tabs = document.querySelectorAll('[data-target]'),
-            tabContents = document.querySelectorAll('[data-content]')
+/* REMOVER MENU MOBILE */
 
-    tabs.forEach(tab =>{
-        tab.addEventListener('click', () =>{
-            const target = document.querySelector(tab.dataset.target)
+const navLink = document.querySelectorAll('.nav__link')
 
-            tabContents.forEach(tabContent =>{
-                tabContent.classList.remove('qualification__active')
-            })
-            target.classList.add('qualification__active')
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // Esconder menu quando clica em algum link
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-            tabs.forEach(tab =>{
-                tab.classList.remove('qualification__active')
-            })
+/* FUNÇÃO ABRIR SKILLS  */
+const skillsContent = document.getElementsByClassName('skills__content'),
+    skillsHeader = document.querySelectorAll('.skills__header')
 
-            tab.classList.add('qualification__active')
-        })
-    })
+function toggleSkills() {
+    let itemClass = this.parentNode.className
 
-    /* SERVIÇOS MODAL */ 
-    const modalViews = document.querySelectorAll('.services__modal'),
-            modalBtns = document.querySelectorAll('.services__button'),
-            modalCloses = document.querySelectorAll('.services__modal-close')
-
-    let modal = function(modalClick){
-        modalViews[modalClick].classList.add('active-modal')
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close'
     }
 
-    modalBtns.forEach((modalBtn, i) => {
-        modalBtn.addEventListener('click', ()=>{
-            modal(i)
-        })
-    })
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
 
-    modalCloses.forEach((modalClose) => {
-        modalClose.addEventListener('click', () =>{
-            modalViews.forEach((modalView) =>{
-                modalView.classList.remove('active-modal')
-            })
+skillsHeader.forEach((el) => {
+    el.addEventListener('click', toggleSkills)
+})
+
+/* GUIAS DE QUALIFICAÇÕES */
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification__active')
+        })
+
+        tab.classList.add('qualification__active')
+    })
+})
+
+/* SERVIÇOS MODAL */
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
         })
     })
+})
+
+
+/* PORTFOLIO SWIPER */
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
